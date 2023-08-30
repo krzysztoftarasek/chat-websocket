7378b77cf4def54ed9008cc6697577eeaacf1e96
Krzysztof T <krzysztof.tarasek@l4web.pl>
2023-08-30 12:12:56 +0200

    first commit

---
+++ bin/chat-server.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+use Ratchet\Server\IoServer;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
+use WebsocketSample\Chat;
+
+  require dirname(__DIR__) . '/vendor/autoload.php';
+
+  $server = IoServer::factory(
+    new HttpServer(
+      new WsServer(
+        new Chat()
+      )
+    ),
+    8080
+  );
+
+  $server->run();
